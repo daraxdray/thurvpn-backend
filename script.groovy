3f3687314854f78bd2666c4491debeac5b680315
Ubuntu <ubuntu@ip-172-31-24-7.us-west-1.compute.internal>
2023-03-09 19:09:58 +0000

    Add image name

--- script.groovy
+++ script.groovy
@@ -26,8 +26,8 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the image...'
-    sh 'kubectl apply -f kubernetes/deployment.yaml'
-    sh 'kubectl apply -f kubernetes/service.yaml'
+    sh 'envsubst < kubernetes/deployment.yaml | kubectl apply -f -'
+    sh 'envsubst < kubernetes/service.yaml | kubectl apply -f -'
 } 
 
 def commitVisioning() {
