1ebfc2da8bade90fba6747603d6fa6cd852e8560
Emmanuel Oga <emmanuel.oga@korsgy.com>
2023-06-08 08:15:12 +0000

    Update file script.groovy

--- script.groovy
+++ script.groovy
@@ -16,13 +16,19 @@ def buildApp() {
 } 
 
 def buildImage() {
-      echo "building the docker image..."
-      withCredentials([usernamePassword(credentialsId: 'thurvpnapi-ecr-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-          sh "docker build -t ${IMAGE_REPO}:${imageVersion} ."
-          sh "echo $PASS | docker login -u $USER --password-stdin ${ECR_REPO_URL}"
-          sh "docker push ${IMAGE_REPO}:${imageVersion}"
-      }
+    echo "building the docker image..."
+    sh "docker build -t ${IMAGE_REPO}:${imageVersion} ."
 } 
+
+def pushImage(){
+    withAWS(credentials: 'emmanuel-aws-credentials', region: 'us-west-1'){
+        echo 'deploying docker image to aws ecr...'
+        sh 'aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 844268948863.dkr.ecr.us-west-1.amazonaws.com'
+        echo 'push the image to ecr'
+        sh "docker push ${IMAGE_REPO}:${imageVersion}"
+    }
+}
+
 
 def deployApp() {
     echo 'deploying the image...'
