4c8c404f7637aaeb5ee58becb0cd21c2e2e41304
STEPHEN IGNATIUS <stephen.ignatius@gmail.com>
2023-06-26 16:00:16 +0100

    faqs

--- routes/purchases.js
+++ routes/purchases.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const {authorization, authMiddleware,authAdmin} = require('../middleware/authentication')
-const {createPurchase,getPurchaseById,getPurchaseByUserId,getAllPurchases,createStripeSheet} = require('../controllers/purchases')
-=======
 const {authMiddleware,authAdmin} = require('../middleware/authentication')
 const {createPurchase,getPurchaseById,getPurchaseByUserId,getAllPurchases,createStripeSheet, deletePurchase} = require('../controllers/purchases')
->>>>>>> 0b42c66cfe7c11627cdd12c9ea882c501118404a
 
 
 
